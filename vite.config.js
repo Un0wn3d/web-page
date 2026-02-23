af1bc2f54ee5bf2669bb2de875915b4f87603584
Un0wn3d <Vadim.Besedovskyi@gmail.com>
2026-02-23 19:17:33 +0100

    2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Змініть base на назву вашого репозиторію для GitHub Pages
-// Наприклад: base: '/eco-toloka/'
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/web-page/',
 })
