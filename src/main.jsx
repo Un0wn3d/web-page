98f117d3b791a19c7f81c254e9955e313a8678e6
Un0wn3d <Vadim.Besedovskyi@gmail.com>
2026-02-23 19:38:29 +0100

    3

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from '/store/index.js'
+import { store } from '/store'
 import './index.css'
+import './analytics.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 )
